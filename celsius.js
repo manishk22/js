5336f61cc24f9c21c4c9652e3f4f1af7d95f2bec
Manish <mks4336@gmail.com>
2019-01-01 18:58:58 +0530

    this is for vowel and celcius

---
+++ celsius.js
@@ -0,0 +1,4 @@
+let far=30
+//calcualting the temp in celcius
+let celcius=(((far-32)*5)/9)
+console.log(celcius+'degree celcius');
---
+++ variables.js
@@ -0,0 +1,8 @@
+let name='manish'
+let score=100
+let passing=35
+let extra=score-passing
+console.log(extra)
+let firstname='manish'
+let lastname='kumar'
+console.log(firstname+' '+lastname);
